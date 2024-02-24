311a7f4ace83d9eed6cca5882a3e9b627922b494
Hiz <HizTelban23@hotmail.com>
2024-02-24 23:27:53 +0200

    tester

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            this._storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() {
+            return await _storeContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+    }
+}
